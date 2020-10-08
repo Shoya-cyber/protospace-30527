1b94eeb6b5ff91b60d9fcfa609362b04e7249b11
Shoya-cyber <shoyasan0627@gmail.com>
2020-10-08 15:26:43 +0900

    プロトタイプ編集機能の修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, only: [:create,:edit, :update, :destroy ]
+  before_action :authenticate_user!, only: [:new, :destroy, :edit, :update, :create]
   before_action :move_to_index, only: :edit
   def index
     @prototypes = Prototype.all
@@ -51,7 +51,8 @@ class PrototypesController < ApplicationController
   end
 
   def move_to_index
-    unless current_user == @user
+    @prototype = Prototype.find(params[:id])
+    unless user_signed_in? && current_user.id == @prototype.user_id
       redirect_to action: :index
     end
   end
